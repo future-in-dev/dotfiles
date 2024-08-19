c322b224d231070a98c5b6f2efa28eac246d0318
the-future-in-dev <thefutureinweb@icloud.com>
2024-08-19 11:21:31 +0300

    Disabling luarocks and hererocks in lazy.nvim

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -32,6 +32,11 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  -- disable luarocks
+  rocks = {
+	  enabled = false,
+	  hererocks = false
+  },
 })
 
 -- Setup Mason
