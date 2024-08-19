b7f8459bec3b425543244749c5c96f86f812205b
the-future-in-dev <thefutureinweb@icloud.com>
2024-08-19 19:25:46 +0300

    Adding rustaceanvim plugin for nvim

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -48,8 +48,12 @@ vim.cmd("set shiftwidth=2")
 vim.cmd.colorscheme "gruvbox-baby" -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 
 -- Setup Telescope
+-- Source: https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#usage
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- Setup Neotree
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -27,5 +27,11 @@ return {
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
+  },
+  -- Source: https://github.com/mrcjkb/rustaceanvim
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
   }
 }
