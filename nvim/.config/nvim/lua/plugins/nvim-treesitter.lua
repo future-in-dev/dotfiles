182233f5f9fd269456b9b697ef0ee8cb4bf295d0
the-future-in-dev <thefutureinweb@icloud.com>
2024-08-19 12:57:24 +0300

    Adding nvim-treesitter

---
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,10 @@
+-- Source: https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation
+
+local M = {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+}
+
+return { M }
