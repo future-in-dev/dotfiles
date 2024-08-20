17ef8911d362d3620b67955c3d8b50f28014fb4e
the-future-in-dev <thefutureinweb@icloud.com>
2024-08-20 08:34:09 +0300

    Archiving custom nvim config to replace with lazynvim distro

--- nvim/.config/nvim/lua/plugins/commentary.vim.lua
+++
@@ -1,6 +0,0 @@
--- Source: https://github.com/tpope/vim-commentary
--- Guide: https://www.youtube.com/watch?v=WFGMUf8lTo8&list=PLsz00TDipIfd500OiPVRyZIc9TLHRRgIM&index=2
-
-return {
-  'tpope/vim-commentary',
-}
